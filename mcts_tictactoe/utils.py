872f3430f54f1b89989047671cd787448de25e1e
Israel Oluwajuwon Adewuyi <i.adewuyi@innopolis.university>
2025-04-28 11:20:24 +0300

    generate board with probability

--- mcts_tictactoe/utils.py
+++ mcts_tictactoe/utils.py
@@ -3,9 +3,10 @@ from typing import List
 
 def generate_random_board(include_space: bool = False, seed: int =42) -> List[str]:
     # random.seed(seed)
-    chars = ["O", "X"]
+    chars = ["O", "X", " "]
+    probabilities = [0.3, 0.3, 0.4]
     if include_space:
         chars.append(" ")
-    board = random.choices(chars, k=9)
+    board = random.choices(chars, weights=probabilities, k=9)
 
     return board
